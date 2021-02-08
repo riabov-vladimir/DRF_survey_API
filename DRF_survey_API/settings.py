174e38f3d4edaa546ffdc25b598837e66fce1a85
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2021-02-08 19:14:38 +0300

    set up

--- DRF_survey_API/settings.py
+++ DRF_survey_API/settings.py
@@ -120,3 +120,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django.db': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
